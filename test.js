0bb56c7eacf0edb9d481c9b2eab1e338f512b5ae
glowlex <navsl@yandex.ru>
2016-10-03 02:47:14 +0300

    test

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 let assert = require('assert');
 let hello = require('./public/main').hello;
-let filter = require('./public/lib/main').filter;
+let filter = require('./public/lib/filter').filter;
 
 
 assert.equal(filter('orange', ['orange']), '******');
